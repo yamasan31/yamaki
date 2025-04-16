d1eb6a74f539a05dd7a96f2e322a9cd3c1ecf971
yamaki <yamaguchirei3@gmail.com>
2025-04-16 18:14:28 +0900

    hn

--- src/app/user/register/page.js
+++ src/app/user/register/page.js
@@ -1,16 +1,46 @@
-import MyPage from "./myPage"
+"use client" 
+import { useState } from "react"
 
-export async function generateMetadata(context) {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/readsingle/${context.params.id}`, {cache: 'no-store'})
-    const jsonData = await response.json()
-    const singleItem = jsonData.singleItem
-    return {
-        title: singleItem.title,
-        description: singleItem.description
+const Register = () => {
+    const [name, setName] = useState("") 
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()  
+        try{
+            const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/user/register`, {
+                method: "POST",
+                headers: { 
+                    "Accept": "application/json", 
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ 
+                    name: name,
+                    email: email,
+                    password: password
+                })
+            }) 
+            const jsonData = await response.json() 
+            alert(jsonData.message) 
+        }catch{
+            alert("ユーザー登録失敗") 
+        }
     }
-}
-const UpdateItem = (context) => {
-    return<MyPage {...context}/>   
+
+    return (
+        <div>
+            <title>登録ページ</title>     
+            <meta name="description" content="登録ページです"/>
+            <h1 className="page-title">ユーザー登録</h1>
+            <form onSubmit={handleSubmit}>
+                <input value={name} onChange={(e) => setName(e.target.value)} type="text" name="name" placeholder="名前" required/> 
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" name="email" placeholder="メールアドレス" required/>
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="text" name="password" placeholder="パスワード" required/>
+                <button>登録</button>
+            </form> 
+        </div>
+    )
 }
 
-export default UpdateItem
+export default Register
